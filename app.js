f59b7b09ba7924bfae6f4d8ee26846a9ac02fcbb
Darshil <ldarshill@gmail.com>
2023-02-25 16:08:08 +0530

    simple node server serving html and css files

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("App js");
+const http = require("http");
+const { readFileSync } = require("fs");
+
+const home = readFileSync("./index.html", "utf-8");
+const homeStyles = readFileSync("./index.css", "utf-8");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(home);
+    res.end();
+  } else if (url === "/index.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/plain" });
+    res.end("Page not found");
+  }
+});
+
+server.listen(5000);
